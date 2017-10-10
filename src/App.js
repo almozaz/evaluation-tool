b5e546be315673ce538c49b30d7a39cea8f91952
almozaz <alacruzmozaz@gmail.com>
2017-10-10 10:32:56 +0200

    Separate main contaiers in different routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react'
-import Navigation from './components/Navigation'
 import './App.css'
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Navigation />
         { this.props.children }
       </div>
     )
